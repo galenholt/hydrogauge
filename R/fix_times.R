e58a98928b162de1f7faa6c5afb001a029f00f7d
Galen Holt <53928785+galenholt@users.noreply.github.com>
2022-11-30 14:47:36 +1100

    handle different time formats to start and end

---
+++ R/fix_times.R
@@ -0,0 +1,27 @@
+#' Creates 14-digit character string needed for times in the API
+#'
+#' @param usertime time value from user. may be date, numeric, or character
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fix_times <- function(usertime) {
+
+  # accept a few formats for the start_time and end_time
+  # it wants a 14-character long string, e.g. '19750510000000'
+
+  # let's work with characters. this should turn numeric or date objects into
+  # strings and just pass strings around
+  timestring <- as.character(usertime)
+  collapsetime <- timestring |>
+    stringr::str_extract_all(pattern = '[0-9]', simplify = TRUE) |>
+    stringr::str_flatten()
+  # why do this, and not not flatten? because it works to pad character input
+  startlen <- nchar(collapsetime)
+  if (startlen < 8) {rlang::warn("time specified is not long enough to give a day. Padding with zeros, but may not work.")}
+
+  pad <- rep('0', 14-startlen) |> stringr::str_flatten()
+
+  clean_time <- paste0(collapsetime, pad)
+}
