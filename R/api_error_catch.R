57ea2c2b4830ffe6708a253794c4cdbb1eb9b113
Galen Holt <53928785+galenholt@users.noreply.github.com>
2022-11-30 22:29:18 +1100

    catch some errors that make it into the trace body

--- R/api_error_catch.R
+++ R/api_error_catch.R
@@ -2,7 +2,26 @@ api_error_catch <- function(bodylist, call = rlang::caller_env()) {
   e_value <- bodylist[1]
 
   if (e_value != 0) {
-    ermessage = paste0("API error number ", e_value, "\nMessage: ", bodylist$error_msg)
+    ermessage = paste0("API error number ", e_value, ". Message: ", bodylist$error_msg)
     rlang::abort(message = ermessage, call = call)
   }
+}
+
+ts_error_catch <- function(bodytib, call = rlang::caller_env()) {
+
+  # if no errors, return
+  if (all(bodytib$error_num == 0)) {return()}
+
+  bodytib |>
+    dplyr::filter(error_num != 0) |>
+    dplyr::select(error_num, error_msg, site, variable) |>
+    dplyr::distinct()
+
+  ermessage = paste0("API error number ", bodytib$error_num,
+                     ". Message: ", bodytib$error_msg)
+  ermessage = stringr::str_flatten(ermessage, collapse = "\n")
+
+  rlang::abort(message = ermessage, call = call)
+
+
 }
